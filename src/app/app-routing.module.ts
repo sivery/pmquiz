c2362a540e2e38cf6b28d02a60d35f05c82381ee
Xavier Reuter <xavier.reuter.ext@eurocontrol.int>
2023-02-14 00:32:17 +0100

    remove uneeded subfolder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {FormComponent} from "./form/form.component";
+import {PretextComponent} from "./pretext/pretext.component";
+import {QuizComponent} from "./quiz/quiz.component";
+import {Route} from "./core";
+import {CanDeactivateQuizGuard} from "./quiz/can-deactivate-quiz.guard";
+import {ResultsComponent} from "./results/results.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  Route.withShell([
+    {path: '', component: FormComponent},
+    {path: 'preambule', component: PretextComponent},
+    {path: 'quiz', component : QuizComponent, data: { responses: false }, canDeactivate: [CanDeactivateQuizGuard]},
+    {path: 'resultats', component: ResultsComponent},
+    {path: 'reponses', component: QuizComponent, data: { responses: true }, canDeactivate: [CanDeactivateQuizGuard]},
+    { path: '**', redirectTo: '/', pathMatch: 'full' },
+  ])
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,55 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {ConfirmationService} from "primeng/api";
+import {QuizComponent} from "./quiz/quiz.component";
+import {PretextComponent} from "./pretext/pretext.component";
+import {FormComponent} from "./form/form.component";
+import {ResultsComponent} from "./results/results.component";
+import {FormsModule} from "@angular/forms";
+import {CoreModule} from "./core";
+import {ButtonModule} from "primeng/button";
+import {MultiSelectModule} from "primeng/multiselect";
+import {AccordionModule} from "primeng/accordion";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {RadioButtonModule} from "primeng/radiobutton";
+import {PanelModule} from "primeng/panel";
+import {DeferModule} from "primeng/defer";
+import {ConfirmDialogModule} from "primeng/confirmdialog";
+import {InputTextModule} from "primeng/inputtext";
+import {InputNumberModule} from "primeng/inputnumber";
+import {TableModule} from "primeng/table";
+import {MessagesModule} from "primeng/messages";
+import {PretextFilterPipe} from "./pretext/pretext-filter.pipe";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuizComponent,
+    PretextFilterPipe,
+    PretextComponent,
+    FormComponent,
+    ResultsComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    CoreModule,
+    AppRoutingModule,
+    ButtonModule,
+    MultiSelectModule,
+    AccordionModule,
+    PanelModule,
+    BrowserAnimationsModule,
+    RadioButtonModule,
+    DeferModule,
+    ConfirmDialogModule,
+    InputTextModule,
+    InputNumberModule,
+    TableModule,
+    MessagesModule
   ],
-  providers: [],
+  providers: [ConfirmationService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
